28224cac482d5944314cff29ad015fe9d7b6a231
Ivan <ivankonovalov96@inbox.ru>
2025-02-25 16:49:19 +0500

    feat: add banner and mission block

--- htdev/src/js/image.js
+++ htdev/src/js/image.js
@@ -1,3 +1,5 @@
 import favicon from "../assets/image/favicon-duffbeer.png";
-import heroBg from "../assets/image/hero-bg.png";
+import heroBg from "../assets/image/hero-bg.webp";
 import aboutBg from "../assets/image/about-decor-optimized.svg";
+import bannerBg from "../assets/image/banner-bg-optimized.svg";
+import missionBg from "../assets/image/mission-bg.webp";
