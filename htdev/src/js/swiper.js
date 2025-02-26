aade723e9be6af1458b5025fe662400ce3d9593a
Ivan <ivankonovalov96@inbox.ru>
2025-02-26 10:41:18 +0500

    feat: add duffbeer block

--- htdev/src/js/swiper.js
+++ htdev/src/js/swiper.js
@@ -12,7 +12,7 @@ function getAdvantagesSwiper() {
     spaceBetween: 25,
 
     pagination: {
-      el: ".swiper-pagination",
+      el: ".advantages__pagination",
       clickable: "true",
     },
 
@@ -34,4 +34,36 @@ function getAdvantagesSwiper() {
   });
 }
 
+function getDuffbeersSwiper() {
+  console.log("Ok");
+  new Swiper(".duffbeer__box", {
+    // direction: "horizontal",
+    // loop: true,
+    slidesPerView: 2,
+    spaceBetween: 25,
+
+    pagination: {
+      el: ".duffbeer__pagination",
+      clickable: "true",
+    },
+
+    navigation: {
+      nextEl: ".duffbeer__btn--next",
+      prevEl: ".duffbeer__btn--prev",
+    },
+
+    // breakpoints: {
+    //   0: {
+    //     spaceBetween: 25,
+    //   },
+
+    //   700: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 0,
+    //   },
+    // },
+  });
+}
+
 mediaQueryS.addEventListener("change", getAdvantagesSwiper);
+mediaQueryS.addEventListener("change", getDuffbeersSwiper);
